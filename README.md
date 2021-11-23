19118c7f787ed88fd262232504fb7beb4a93c429
徐志勇 <xuzhiyong@whty.com.cn>
2021-11-23 15:38:27 +0800

    添加cli脚本功能

--- README.md
+++ README.md
@@ -2,4 +2,26 @@
 
 [![TEST](https://github.com/JohnieXu/75_action/actions/workflows/run.yml/badge.svg)](https://github.com/JohnieXu/75_action/actions/workflows/run.yml)
 
-这是一个自动抓取奇舞周刊全部文章的脚本，支持使用 GitHub action 自动抓取
+> 这是一个自动抓取奇舞周刊全部文章的脚本，支持使用 GitHub action 自动抓取
+
+## 自动抓取
+
+支持配置github action自动抓取数据，并保存抓取的数据到artifcat
+## cli工具使用
+### 随机返回一篇文章链接
+
+```bash
+npx 75_action random
+```
+
+### 随机返回n篇文章链接
+
+```bash
+npx 75_action random [n] // n为任意大于0的数（当总文章数小于n时返回全部文章链接）
+```
+
+### 手动刷新文章数据
+
+```bash
+npx 75_action fetch
+```
---
+++ bin/75_action.js
@@ -0,0 +1,72 @@
+#!user/bin/env node
+
+const program = require('commander')
+const packageJson = require('../package.json')
+const fetch = require('../lib/fetch')
+
+function random(number, list) {
+  const min = 0
+  const max = list.length
+  const rand = () => {
+    const r = Math.round(Math.random() * max)
+    if (r < min || r > max) {
+      return rand()
+    }
+    return r
+  }
+  const res = []
+  const randI = []
+  for (let i = 0; i < number; i++) {
+    let r = rand()
+    randI.push(r)
+    res.push(list[r])
+  }
+  return res
+}
+
+/**
+ * 75_action -h // 帮助说明
+ * 75_action -v // version
+ * 75_action -d // debug
+ * 75_action // 随机1篇文章（暂未实现）
+ * 7t_action // 帮助说明
+ * 75_action random [number] // 随机n篇文章
+ * 75_action fetch // 抓取全部文章
+ */
+
+program.version(`75_action ${packageJson.version}`)
+       .usage('[<command> [options]]')
+
+program.command('random [number]')
+       .description('随机获取n篇文章链接')
+       .option('-d, --debug', '开启debug模式')
+       .action((number, options) => {
+         number = number || 1
+         if (options.debug) {
+           console.log(number, options)
+         }
+         fetch({ save: 'home', progress: true }).then(({ collections, articles }) => {
+           const selected = random(number, articles)
+           console.log(JSON.stringify(selected, null, 2))
+           process.exit()
+         }).catch((e) => {
+           console.log(e)
+           process.exit(1)
+         })
+       })
+
+program.command('fetch')
+       .description('重新抓取文章链接')
+       .option('-d, --debug', '开启debug模式')
+       .action((options) => {
+          if (options.debug) {
+            console.log(options)
+          }
+          fetch({ save: 'home', progress: true, reload: true }).then(({ collections, articles }) => {
+            console.log(`抓取完成，总共${collections.length}个集合，${articles.length}篇文章`)
+            process.exit()
+          })
+       })
+
+program.parse(process.argv)
+       
